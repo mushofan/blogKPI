062de242b0973f443528feea205db90cfa031c63
fahziar <fahziar_rw@yahoo.co.uk>
2016-02-25 14:27:04 +0700

    sql dengan tabel image

--- simpleblog.sql
+++ simpleblog.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.7.1
+-- version 4.5.1
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 14, 2014 at 08:05 AM
--- Server version: 5.5.40
--- PHP Version: 5.5.15
+-- Generation Time: Feb 25, 2016 at 05:49 AM
+-- Server version: 10.1.9-MariaDB
+-- PHP Version: 5.6.15
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,13 +14,37 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `simpleblog`
 --
-CREATE DATABASE IF NOT EXISTS `simpleblog` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `simpleblog`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `images`
+--
+
+DROP TABLE IF EXISTS `images`;
+CREATE TABLE `images` (
+  `id` int(11) NOT NULL,
+  `judul` text NOT NULL,
+  `gambar` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Truncate table before insert `images`
+--
+
+TRUNCATE TABLE `images`;
+--
+-- Dumping data for table `images`
+--
+
+INSERT INTO `images` (`id`, `judul`, `gambar`) VALUES
+(1, 'hello world', 'images/img_Sdpkzom2KEO8dLsEXZeWbleC_56ce61d257d0f6.45689015.jpg'),
+(2, 'second', 'images/img_KPoZJevh24WR8KHecnq6zqQW_56ce6765eb0bb9.49378235.jpg');
 
 -- --------------------------------------------------------
 
@@ -29,15 +53,20 @@ USE `simpleblog`;
 --
 
 DROP TABLE IF EXISTS `komentar`;
-CREATE TABLE IF NOT EXISTS `komentar` (
-`id` int(11) NOT NULL,
+CREATE TABLE `komentar` (
+  `id` int(11) NOT NULL,
   `id_post` int(11) NOT NULL,
   `nama` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `tanggal` date NOT NULL,
   `komentar` text NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=13 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Truncate table before insert `komentar`
+--
+
+TRUNCATE TABLE `komentar`;
 -- --------------------------------------------------------
 
 --
@@ -45,43 +74,105 @@ CREATE TABLE IF NOT EXISTS `komentar` (
 --
 
 DROP TABLE IF EXISTS `posts`;
-CREATE TABLE IF NOT EXISTS `posts` (
-`id` int(11) NOT NULL,
+CREATE TABLE `posts` (
+  `id` int(11) NOT NULL,
   `judul` text NOT NULL,
   `tanggal` date NOT NULL,
   `konten` text NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=30 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Truncate table before insert `posts`
+--
+
+TRUNCATE TABLE `posts`;
+--
+-- Dumping data for table `posts`
+--
+
+INSERT INTO `posts` (`id`, `judul`, `tanggal`, `konten`) VALUES
+(5, 'first', '2016-11-11', 'first post');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Truncate table before insert `user`
+--
+
+TRUNCATE TABLE `user`;
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `password`) VALUES
+(1, 'oji', '$2a$10$TQIumrOJlTdhaYMeQZWxx.6bPsrjRjNc2fsoAPOeNFpVhmX3.cCqW'),
+(2, 'ojii', '$2y$10$XAnoCseQ/CLmn9g5EtUkeOx.u9EJRAq4EuLSvL1fSkS7in.ufMrEK');
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `images`
+--
+ALTER TABLE `images`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `komentar`
 --
 ALTER TABLE `komentar`
- ADD PRIMARY KEY (`id`), ADD KEY `id_post` (`id_post`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_post` (`id_post`);
 
 --
 -- Indexes for table `posts`
 --
 ALTER TABLE `posts`
- ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT for table `images`
+--
+ALTER TABLE `images`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
 -- AUTO_INCREMENT for table `komentar`
 --
 ALTER TABLE `komentar`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- Constraints for dumped tables
 --
@@ -90,7 +181,7 @@ MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 -- Constraints for table `komentar`
 --
 ALTER TABLE `komentar`
-ADD CONSTRAINT `komentar_ibfk_1` FOREIGN KEY (`id_post`) REFERENCES `posts` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `komentar_ibfk_1` FOREIGN KEY (`id_post`) REFERENCES `posts` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
